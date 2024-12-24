4af0f458f4c79b2618310e523b66b5b6b3028c85
Samiaboug <bougtoubsamia@gmail.com>
2024-12-24 12:55:18 +0000

    save

--- Blog_app/app/Models/Article.php
+++ Blog_app/app/Models/Article.php
@@ -5,10 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Testing\Fluent\Concerns\Has;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 
 class Article extends Model
 {
     //
     use HasFactory;
     protected $fillable = ['title', 'content', 'author'];
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
